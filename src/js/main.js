484f127367c8e31d8b33ebc0ff07a7f522cb3538
zhoushijie <1127284964@qq.com>
2018-10-19 17:09:08 +0800

    1.webpack初步完善 2.css剥离

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import MikuBack from './back'
 import MikuAudio from './audio'
 import MikuCont from './cont'
-import {BACK_LINES,SHOW_LINES} from './res'
+import { BACK_LINES, SHOW_LINES } from './res'
+
+import '../css/reset.css'
+import '../css/main.less'
 
 let bw = Math.floor(window.innerWidth);
 let bh = Math.floor(window.innerHeight);
@@ -12,89 +15,88 @@ board.width = bw;
 board.height = bh;
 let bctx = board.getContext('2d');
 
-let back = new MikuBack(bw,bh);
-let cont = new MikuCont(bw,bh);
+let back = new MikuBack(Math.floor(bw / 2), Math.floor(bh / 2));
+let cont = new MikuCont(bw, bh);
 let backas = addBackAuido();
 let bindex = 0;
 let showas = addShowAudio();
 
 addBox();
 back.init();
-setInterval(()=>{
+renderCtx();
+
+setInterval(() => {
     back.addSwitch(60);
-},2000);
-setInterval(()=>{
+}, 2000);
+setInterval(() => {
     backas[bindex].audio.play();
     // bindex = Math.floor(Math.random()*backas.length);
     bindex++;
-    if(bindex>=backas.length) bindex=0;
-},200);
+    if (bindex >= backas.length) bindex = 0;
+}, 200);
 
-renderCtx();
-
-
-board.addEventListener('click',()=>{
+board.addEventListener('click', () => {
     cont.addShape('type_4');
 })
 let lastIndex = 0;
 let mouseDown = false;
-container.addEventListener('mousedown',e=>{
+container.addEventListener('mousedown', e => {
     let sindex = e.target.getAttribute('data-show');
-    if(sindex===lastIndex) return;
+    if (sindex === lastIndex) return;
     showas[sindex].audio.play();
     lastIndex = sindex;
-    let stype = sindex*1%4+1;
+    let stype = sindex * 1 % 4 + 1;
     cont.addShape(`type_${stype}`);
     lastIndex = sindex;
     mouseDown = true;
 })
-container.addEventListener('mousemove',e=>{
-    if(!mouseDown) return;
+container.addEventListener('mousemove', e => {
+    if (!mouseDown) return;
     let sindex = e.target.getAttribute('data-show');
-    if(sindex===lastIndex) return;
+    if (sindex === lastIndex) return;
     showas[sindex].audio.play();
     lastIndex = sindex;
-    let stype = sindex*1%4+1;
+    let stype = sindex * 1 % 4 + 1;
     cont.addShape(`type_${stype}`);
 })
-container.addEventListener('mouseup',e=>{
+container.addEventListener('mouseup', e => {
     lastIndex = 0;
     mouseDown = false;
 })
 
-function renderCtx(){
+function renderCtx() {
     clearFrame();
     renderBack();
     renderCont();
     requestAnimationFrame(renderCtx);
 }
 
-function clearFrame(){
-    bctx.clearRect(0,0,bw,bh);
+function clearFrame() {
+    bctx.clearRect(0, 0, bw, bh);
 }
 
-function renderBack(){
-    bctx.drawImage(back.canvas,0,0,bw,bh);
+function renderBack() {
+    bctx.drawImage(back.canvas, 0, 0, bw, bh);
 }
 
-function renderCont(){
+function renderCont() {
     cont.contUpdate();
-    bctx.drawImage(cont.canvas,0,0,bw,bh);
+    bctx.drawImage(cont.canvas, 0, 0, bw, bh);
 }
 
-function addBox(){
+function addBox() {
     let count = 32;
-    for(let i =0;i<count;i++){
+    for (let i = 0; i < count; i++) {
         let div = document.createElement('div');
         div.className = "box";
-        div.setAttribute('data-show',i);
+        div.setAttribute('data-show', i);
         container.appendChild(div);
     }
 }
 
-function addBackAuido(){
-    let arr =[];
-    for(let i in BACK_LINES){
+function addBackAuido() {
+    let arr = [];
+    for (let i in BACK_LINES) {
         let src = BACK_LINES[i];
         let obj = {};
         obj.index = i;
@@ -103,9 +105,10 @@ function addBackAuido(){
     }
     return arr;
 }
-function addShowAudio(){
-    let arr =[];
-    for(let i in SHOW_LINES){
+
+function addShowAudio() {
+    let arr = [];
+    for (let i in SHOW_LINES) {
         let src = SHOW_LINES[i];
         let obj = {};
         obj.index = i;
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,90 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
-    devtool:'eval-source-map',
-    entry:__dirname + '/src/js/main.js',
-    output:{
-        path:__dirname +'/build',
-        filename:'bundle.js'
+    mode: 'none',
+    devtool: 'eval-source-map',
+    entry: __dirname + '/src/js/main.js',
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
     },
-    devServer:{
-        contentBase:'./build',
-        historyApiFallback:true,
-        inline:true
-    }
+    devServer: {
+        contentBase: './build',
+        stats: { colors: true },
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+        port: 8082
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: 'style-loader',
+                        options: {
+                            singleton: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            importLoaders: true,
+                            minimize: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                        loader: 'style-loader',
+                        options: {
+                            singleton: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: true,
+                            minimize: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'less-loader'
+                    }
+                ],
+                exclude: /node_modules/
+            }
+            
+        ]
+    },
+    optimization: {},
+    plugins: [
+        new CleanWebpackPlugin(['./build']),
+        new webpack.BannerPlugin("Author:Zzuzsj. Thanks for your reading."), //顶部版权实例
+        new HtmlWebpackPlugin({ //启动模板实例
+            template: __dirname + '/src/index.tmpl.html'
+        }),
+        new webpack.HotModuleReplacementPlugin() //热加载插件
+    ]
 }
